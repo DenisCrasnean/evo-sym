792ab77a122f429f3078e56a47532c594a590fdc
Denis Crasnean <deniscrasnean@gmail.com>
2022-03-21 12:45:17 +0200

    Refactored import from programmes api programmes and related

--- src/Command/ImportProgrammeFromApiCommand.php
+++ src/Command/ImportProgrammeFromApiCommand.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace App\Command;
 
-use App\Client\Api\ProgrammeApiClient;
-use App\Entity\Programme;
-use Doctrine\DBAL\Exception;
+use App\Client\Api\ApiClientInterface;
+use App\Controller\Dto\DtoInterface;
+use App\Encryption\EncryptionInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -17,48 +17,59 @@ class ImportProgrammeFromApiCommand extends Command
 {
     protected static $defaultName = 'app:programme:import-from-api';
 
-    private ProgrammeApiClient $programmeApiClient;
+    private ApiClientInterface $programmeApiClient;
+
+    private DtoInterface $programmeDto;
+
+    private EncryptionInterface $caesarEncryption;
 
     private EntityManagerInterface $entityManager;
 
     public function __construct(
-        ProgrammeApiClient $programmeApiClient,
+        ApiClientInterface $programmeApiClient,
+        DtoInterface $programmeDto,
+        EncryptionInterface $caesarEncryption,
         EntityManagerInterface $entityManager
     ) {
         $this->programmeApiClient = $programmeApiClient;
+        $this->programmeDto = $programmeDto;
+        $this->caesarEncryption = $caesarEncryption;
         $this->entityManager = $entityManager;
 
         parent::__construct();
     }
 
+    /**
+     * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface
+     */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $inputOutput = new SymfonyStyle($input, $output);
-
         $response = $this->programmeApiClient->fetch('GET', 'sport-programs');
-        $responseBody = $response->toArray();
+        $response = $response->toArray();
+        $decryptedData = [];
 
-        $decryptResponse = new CaesarDecryption();
+            foreach ($response['data'] as $data) {
+                $decryptedDataKeys = [];
+                $decryptedDataValues = [];
 
-        //  TODO
-        //  Implement a service to map objet to response data
-        foreach ($responseBody['data'] as $data) {
-            $name = $decryptResponse->decipher($data['name'], 8);
-            $description = $decryptResponse->decipher($data['description'], 8);
+                foreach ($data as $key => $value) {
+                    $decryptedDataKeys[] = $key;
+                    $decryptedDataValues[] = $this->caesarEncryption->decrypt((string) $value);
+                }
 
-            $programme = new Programme();
-            $programme->name = $name;
-            $programme->description = $description;
-            $programme->setStartTime(new \DateTime($data['startDate']));
-            $programme->setEndTime(new \DateTime($data['endDate']));
-            $programme->setIsOnline($data['isOnline']);
-
-            try {
-                $this->entityManager->persist($programme);
-                $this->entityManager->flush();
-            } catch (Exception $e) {
-                $e->getMessage();
+                $decryptedData[] = array_combine($decryptedDataKeys, $decryptedDataValues);
             }
+
+        $programmes = $this->programmeDto->fromArrayCollection($decryptedData);
+
+        foreach ($programmes as $programme) {
+            $this->entityManager->persist($programme);
+            $this->entityManager->flush();
         }
 
         $inputOutput->success('Programmes imported successfully!');
