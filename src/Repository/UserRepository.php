47dbaa1dedf6c949422dff3aca3f380fa5ee6fc2
Denis Crasnean <deniscrasnean@gmail.com>
2022-04-07 10:25:25 +0300

    I had to do this in order to be able to merge to master the complete UserREpository

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -15,31 +15,5 @@ class UserRepository extends ServiceEntityRepository
         parent::__construct($registry, User::class);
     }
 
-    /**
-     * @throws NonUniqueResultException
-     * @throws NoResultException
-     */
-    public function findByEmail(string $email): User
-    {
-        return $this->createQueryBuilder('u')
-           ->where('u.email = :email')
-           ->setParameter(':email', $email)
-           ->getQuery()
-           ->getSingleResult();
-    }
 
-    /**
-     * @throws NonUniqueResultException
-     * @throws NoResultException
-     */
-    public function findByPasswordResetToken(string $token): User
-    {
-        return $this->createQueryBuilder('u')
-            ->select('u')
-            ->from('App:PasswordReset', 'pr')
-            ->where('pr.token = :token')
-            ->setParameter('token', $token)
-            ->getQuery()
-            ->getSingleResult();
-    }
 }
